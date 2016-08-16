058ad9cec979331437656041e19de8ac8762e51b
Gosha Fighten <goshafighten@gmail.com>
2016-08-17 00:57:33 +0300

    #1 preview

--- getDXSandbox.js
+++ getDXSandbox.js
@@ -2,12 +2,32 @@
 
 var spawn = require('child_process').spawn;
 const path = require('path');
+var prompt = require('prompt');
 
 var argv = require('minimist')(process.argv.slice(1));
 var commands = argv._;
 
+var schema = {
+    properties: {
+        destination: {
+            required: true
+        }
+    }
+};
+
 create(commands[0], commands[1]);
 
+function run(root, destination) {
+    const bat = spawn('cmd.exe', ['/c', path.join(path.dirname(root), `getDXAngular2.bat ${destination}`)], { stdio: 'inherit' });
+}
+
 function create(root, destination) {
-    const bat = spawn('cmd.exe', ['/c', path.join(path.dirname(root), `getDXSandbox.bat ${destination}`)], { stdio: 'inherit' });
+    if (!destination) {
+        prompt.start();
+        prompt.get(schema, function (err, result) {
+            destination = result.destination;
+            run(root, destination);
+        })
+    };
+
 }
