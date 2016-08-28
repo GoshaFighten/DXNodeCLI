5534f5dbc267d4130ee3ef65ee275884820a52ed
Gosha Fighten <goshafighten@gmail.com>
2016-08-29 00:07:21 +0300

    Closes #7

--- dxJquery/code.js
+++ dxJquery/code.js
@@ -1,3 +1,6 @@
+/// <reference path="C:/Program Files (x86)/DevExpress 16.1/DevExtreme/Sources/Lib/ts/jquery.d.ts" />
+/// <reference path="C:/Program Files (x86)/DevExpress 16.1/DevExtreme/Sources/Lib/ts/dx.all.d.ts" />
+
 $(function () {
     $("#btn").dxButton({
         text: "Click",
